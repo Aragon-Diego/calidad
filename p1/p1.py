5d90698633b5940f8378b51102069ee9058fd8c2
Aragon-Diego <diego.aragonCursos@gmail.com>
2020-02-17 12:42:19 -0600

    p1 con estadares

--- p1/p1.py
+++ p1/p1.py
@@ -1,33 +1,67 @@
+"""
+Numero de programa:02
+Nombre:Diego Alonso Aragón Villarreal
+Matricula:361349
+Fecha ultima de modificación:17/02/2020
+Razón de modificación:estandar de codificacion
+"""
+"""
+Clases:
+    Lista():
+        agregar
+        sumatoria   
+        calMedia
+        sumatoriaMedia
+        calDs
+"""
 import math
-"""
-clase lista dinamica
-"""
 
 class Lista:
-    n=0
+    numero=0
     lista=[]
-    def agregar(self,num):
-        self.lista.append(num)
-        self.n+=1
+
+    def agregar(self,numero):
+        """
+            Esta funcion resive un numero que se agregará a la lista dinamica
+            no regresa nada
+        """
+        self.lista.append(numero)
+        self.numero+=1
 
     def sumatoria(self):
+        """
+            Esta funcion hace una sumatoria de todos los elementos de la lista
+            regresa el resultado de la sumatroia
+        """
         total=0
         for i in self.lista:
             total+=i
         return total
 
     def calMedia(self):
-        return (self.sumatoria()/self.n)
+        """
+            Esta funcion calcula la media de la sumatoria
+            regresa la media
+        """
+        return (self.sumatoria()/self.numero)
 
     def sumatoriaMedia(self):
+        """
+            Esta funcion calcula la sumatoria de cada elemento con respecto a la meida
+            regresa la sumatoria
+        """
         total=0
         avg=self.calMedia()
         for i in self.lista:
             total+=(i-avg)**2
         return total
 
-    def calDs(self):
-        return math.sqrt(self.sumatoriaMedia()/(self.n-1))
+    def calDesviacionEstandar(self):
+        """
+            Esta funcion calcula la Desviacion estandar de una sumatoria
+            regresa el calculo
+        """
+        return math.sqrt(self.sumatoriaMedia()/(self.numero-1))
 
 """
 Programa
@@ -40,4 +74,4 @@ if __name__ == "__main__":
             l1.agregar(float(i))
 
     print("Media:"+str(round(l1.calMedia(),2)))
-    print("des. Estandar:"+str(round(l1.calDs(),2)))
+    print("des. Estandar:"+str(round(l1.calDesviacionEstandar(),2)))
