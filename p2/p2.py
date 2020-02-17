f153247daa1a573be066664fc179a2a31b37ad97
Aragon-Diego <diego.aragonCursos@gmail.com>
2020-02-17 08:23:08 -0600

    estructura y comentarios generales terminados

--- p2/p2.py
+++ p2/p2.py
@@ -52,39 +52,49 @@ class Programa:
 
     def programaALineas(self):
         """
-
+            Esta funcion toma el archivo a contar y lo guarda en un arreglo
+            no regresa nada
         """
         pass
     def conteoModificadas(self):
         """
-        
+            Esta funcion es la que cuenta la cantidad de lineas modificadas
+            no regresa nada
         """
         pass
     def conteoBase(self):
         """
-        
+            Esta funcion es la que cuenta la cantidad de lineas base
+            no regresa nada
         """
         pass
     def limpiarLineas(self):
         """
-        
+            Esta funcion lipia el arreglo con las lineas de codigo
+            quitando los comentarios 
+            no regresa nada
         """
         pass
     def conteoTotal(self):
         """
-        
+            cuenta la cantidad de lineas del arreglo con las reglas de conteo
+            no reresa nada
         """
         pass
     def conteoAgregado(self):
         """
-        
+            esta funcion cuenta la cantidad de lineas agregadas en el codigo
+            suma la cantidad de codigo base + codigo modificado y se lo resta al total
+            no regresa nada
         """
         pass
     def filtradoAClases(self):
         """
-        
+            esta funcion es la que toma el arreglo de lineas y lo separa por clases
+            no regresa nada
         """
         pass
+    
 class Clase:
     arrLineasDelArchivo=[]
 
@@ -96,30 +106,22 @@ class Clase:
         
     def setArrLineasDelArchivo(self,arr):
         """
-
+            Esta funcion sirve para guardar el arreglo de lineas del archivo a contar
+            resive el arreglo de string arr
+            no regresa nada.
         """
         self.arrLineasDelArchivo=arr
 
     def regresarCantidadDeFunciones(self):
         """
-
+            esta fucnion hace el conteo de funciones
+            regresa la cantidad de funciones en la clase
         """
         pass
 
     def regresarCantidadDeLineas(self):
         """
-
-        """
-        pass
-
-    def conteoDeLineas(self):
-        """
-
-        """
-        pass
-
-    def conteoDeFunciones(self):
-        """
-
+            esta fucnion hace el conteo de lineas
+            regresa la cantidad de lineas en la clase
         """
         pass
